7aeee24ba8d9e01c157cd13693914c2b355eb186
ErosEros <eros.vittori1994@gmail.com>
2022-10-12 22:12:36 +0200

    stampa nel display il paragrafo 2° con *** per la scritta Marte

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 Creare una variabile con un paragrafo di testo a vostra scelta.
 Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***)
+tutte le occorrenze della parola da censurare. -->
 
 
 
@@ -10,7 +11,10 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 // Creo una variabile con del testo
 $variabileText = "Ogni prodotto che ha bisogno di un manuale per funzionare è rotto. Se le cose non stanno fallendo, non stai innovando abbastanza. Marte è l'unico posto nel sistema solare in cui è possibile che la vita diventi multi-planetaria. Le persone lavorano meglio quando sanno qual'è l obiettivo e perché.";
 
+$badWords = str_replace('Marte', '***', $variabileText);
 ?>
 <!-- creo il paragrafo -->
 <p>Paragrafo: <?php echo $variabileText ?> </p>
+<p>Paragrafo 2nd: <?php echo $badWords ?> </p>
+
 
